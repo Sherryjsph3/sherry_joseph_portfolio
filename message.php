b654afba6ca29ea63e538b592c5aff9485f7d9b4
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-08-11 13:41:17 -0400

    attempt 4 for contact form

--- message.php
+++ message.php
@@ -1,19 +1,19 @@
-
 <?php
 
-if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+$name = $_POST['name'];
+$email= $_POST['emailaddess'];
+$subjectline= $_POST['subject'];
+$message= $_POST['message'];
 
-  $mailTo = "sherryjoseph985@gmail.com";
-  $headers = "From: ".$email;
-  $txt = " You have received an email from ".$name. '.\n\n'.$message;
+$to = "Sherryjoseph985@gmail.com";
 
-  mail($email, $subject, $txt, $headers);
-  header("Location: netlify.app?mailsend");
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject = " . $subjectline . "\r\n Message =" . $message;
+$headers = "From: noreply@inspiring-visvesvaraya-10f1e8.netlify.app" . "\r\n" .
+"CC: ";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
- 
-  
+//redirect
+header("Location:index.html");
 ?>
